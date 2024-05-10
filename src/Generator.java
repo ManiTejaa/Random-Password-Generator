f8fd4d83fc790a73803c87bdaa2f40bbabc37d81
maniteja <51128870+ManiTejaa@users.noreply.github.com>
2024-05-10 14:01:59 +0530

    Third commit

--- src/Generator.java
+++ src/Generator.java
@@ -2,17 +2,6 @@ public class Generator {
     final static String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+\\/~?";
     final static int LENGTH =  80;
 
-//    Scanner sc = new Scanner(System.in);
-//
-//    public Generator(){
-//        requestPassword();
-//    }
-
-//    private void requestPassword() {
-//        System.out.println("Enter the length of the password");
-//        int length = sc.nextInt();
-//        System.out.println("Generated password : " + generatePassword(length));
-//    }
 
     public static String generateRandomPassword(int length) {
         StringBuilder password = new StringBuilder();
